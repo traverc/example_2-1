e6fee4e604f3fd233209158f0f9adfdfda8f675b
Cherrice Traver <traverc@union.edu>
2024-01-10 18:27:25 +0000

    Version 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//Car alarm project with three sensor (button) inputs and four LED 
+//outputs (one off board) and an alarm buzzer.
+
 //=====[Libraries]=============================================================
 
 #include "mbed.h"
@@ -5,35 +8,36 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
-DigitalIn enterButton(BUTTON1);
-DigitalIn gasDetector(D2);
-DigitalIn overTempDetector(D3);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
+DigitalIn ignition(BUTTON1);  
+DigitalIn driverSeat(D4);  
+DigitalIn driverBelt(D5);  
+DigitalIn passengerSeat(D6);
+DigitalIn passengerBelt(D7);
 
-DigitalOut alarmLed(LED1);
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
+DigitalOut alarm(LED3);  // Red
+DigitalOut ignitionEnabled(LED1); // Green
+DigitalOut engineStarted(LED2); // Blue
+DigitalInOut sirenPin(PE_10); 
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 //=====[Declaration and initialization of public global variables]=============
 
-bool alarmState = OFF;
-int numberOfIncorrectCodes = 0;
+bool seatsSecure;
+bool alarmState;
+bool errorPrinted;
 
 //=====[Declarations (prototypes) of public functions]=========================
 
 void inputsInit();
 void outputsInit();
 
-void alarmActivationUpdate();
-void alarmDeactivationUpdate();
+void checkSeats();
+void checkIgnition();
 
-void uartTask();
-void availableCommands();
+void systemGreeting();
+void checkErrors();
+//void availableCommands();
 
 //=====[Main function, the program entry point after power on or reset]========
 
@@ -41,10 +45,11 @@ int main()
 {
     inputsInit();
     outputsInit();
+    systemGreeting();
     while (true) {
-        alarmActivationUpdate(); //adding some comments just to have changes
-        alarmDeactivationUpdate();  //to store to github
-        uartTask();
+        checkSeats(); //
+        checkIgnition();  //
+        checkErrors();
     }
 }
 
@@ -52,68 +57,75 @@ int main()
 
 void inputsInit()
 {
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
+    driverSeat.mode(PullDown);
+    driverBelt.mode(PullDown);
+    passengerSeat.mode(PullDown);
+    passengerBelt.mode(PullDown);
 }
 
 void outputsInit()
 {
-    alarmLed = OFF;
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
+    alarm = OFF;
+    ignitionEnabled = OFF;
+    engineStarted = OFF;
+    sirenPin = ON; //active low
+    errorPrinted = false;
+    alarmState = OFF;
+    seatsSecure = false;
 }
 
-void alarmActivationUpdate()
+void checkSeats()
 {
-    if ( gasDetector || overTempDetector ) {
-        alarmState = ON;
-    }
-    alarmLed = alarmState;
-}
+   if (!engineStarted) {
 
-void alarmDeactivationUpdate()
-{
-    if ( numberOfIncorrectCodes < 5 ) {
-        if ( aButton && bButton && cButton && dButton && !enterButton ) {
-            incorrectCodeLed = OFF;
-        }
-        if ( enterButton && !incorrectCodeLed && alarmState ) {
-            if ( aButton && bButton && !cButton && !dButton ) {
-                alarmState = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
-            }
-        }
-    } else {
-        systemBlockedLed = ON;
-    }
-}
-
-void uartTask()
-{
-    char receivedChar = '\0';
-    if( uartUsb.readable() ) {
-        uartUsb.read( &receivedChar, 1 );
-        if ( receivedChar == '1') {
-            if ( alarmState ) {
-                uartUsb.write( "The alarm is activated\r\n", 24);
-            } else {
-                uartUsb.write( "The alarm is not activated\r\n", 28);
-            }
+        if ( driverSeat && driverBelt && passengerSeat && passengerBelt ) {
+            seatsSecure = true;
+            alarmState = OFF;
         } else {
-            availableCommands();
+            seatsSecure = false;
         }
-    }
+            ignitionEnabled = seatsSecure;
+     }
 }
 
-void availableCommands()
+void checkIgnition()
 {
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '1' to get the alarm state\r\n\r\n", 36 );
+    if ( ignition && !engineStarted ) {
+        if ( ignitionEnabled)  {
+                    ignitionEnabled = OFF;
+                    engineStarted = ON;
+                    uartUsb.write( "Engine Started\r\n", 16);
+        }else{
+                    alarmState = ON;
+        }
+        }
+}
+
+void checkErrors()
+{
+
+    alarm = alarmState;
+    sirenPin = !alarmState;
+    if ( alarmState && !errorPrinted ) {
+        errorPrinted = true;
+        uartUsb.write( "Ignition Inhibited\r\n", 20);
+        if ( !driverSeat) {
+                uartUsb.write( "What? No Driver?\r\n", 18);
+            } 
+        if (!driverBelt) {
+                uartUsb.write( "Driver seatbelt not fastened\r\n",30);
+                    }     
+        if (!passengerSeat) {
+                uartUsb.write("No passenger seated\r\n",21); 
+                        } 
+        if (!passengerBelt) {
+                uartUsb.write( "Passenger seatbelt not fastened\r\n",33);
+                            }
+         }
+  //  alarmState = OFF;
+}
+
+void systemGreeting()
+{
+ uartUsb.write( "\n\nWelcome to enhanced alarm system model 218-W24\r\n", 50 );
 }
